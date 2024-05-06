c4e8e910b1da8c0c9126474e89a65c2eea875c9f
Kollimarla Jagadeep <teja.jagadeep@gmail.com>
2024-05-06 11:12:35 +0530

    jagadeep made changes on  Mon May  6 11:12:35 IST 2024

--- movie-backend/userprofile-service/src/main/java/com/user/userprofileservice/configuration/CorsConfig.java
+++ movie-backend/userprofile-service/src/main/java/com/user/userprofileservice/configuration/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(swaggerUrl)
+                .allowedOrigins(swaggerUrl.split(","))
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
